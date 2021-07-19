7a078a7f792a189f7055cea88a71c3989b27a8cc
jinggege <1878618369@qq.com>
2021-07-19 22:27:21 +0800

    前端初始化

---
+++ repository/src/main/java/com/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author: 陶敬
+ * @date: 2021/7/19 22:07
+ * @description:
+ */
+@Controller
+public class MainController {
+    public String getIndex(){
+        return  "index";
+    }
+
+}
--- src/test/java/com/sale/parent/ParentApplicationTests.java
+++ src/test/java/com/sale/parent/ParentApplicationTests.java
@@ -8,6 +8,7 @@ class ParentApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println(123);
     }
 
 }
